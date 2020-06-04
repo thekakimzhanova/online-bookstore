48579debb28672a1ca852d232c3d4e8fe125fd46
thekakimzhanova <aliya.kakimzhanova@nu.edu.kz>
2020-06-04 15:30:11 +0600

    3rd commit angular

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,12 @@
+export class Book {
+
+    ski: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+}
